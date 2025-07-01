c0e26f30e4f588635eea2050b9ba6390a4a3ed98
Hai123445 <24100185@st.phenikaa-uni.edu.vn>
2025-07-01 09:23:07 +0000

    change gd

--- quan ca phe/src/Giaodich.java
+++ quan ca phe/src/Giaodich.java
@@ -1,24 +1,17 @@
 public class Giaodich {
 
     //bien mieu ta cho giao dich
-    private String MaGiaoDich;
+    String MaGiaoDich;
     String NgayThangNamGiaoDich;
     String GioGiaoDich;
     double TongTien;
 
 
     //phuong thuc hoat dong
-public String setMaGiaoDich(String mgd){
-    MaGiaoDich = mgd;
-    return MaGiaoDich;
+public Giaodich(String mgd,String ngd,String ggd,double tt){
+    this.MaGiaoDich = mgd;
+    this.NgayThangNamGiaoDich = ngd;
+    this.GioGiaoDich = ggd;
+    this.TongTien = tt; 
 }
-public void getMaGiaoDich(){
-    System.out.println("Ma giao dich" + MaGiaoDich);
-}
-    
-public String setNgayThangNamGiaoDich(String ntngd){
-    NgayThangNamGiaoDich = ntngd;
-    return NgayThangNamGiaoDich;
-}
-    
-}
+
