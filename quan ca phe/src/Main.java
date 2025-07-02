49f1b4879b443fed5938f1ab1a43a37f22c22ede
Manx-bit <manx7082@gmail.com>
2025-07-02 07:34:52 +0000

    test

--- quan ca phe/src/Main.java
+++ quan ca phe/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
   public static void main(String[] args) {
     TestUser.Test();
-    TestTime.Test();
     TestRecursion.Test();
     // testKhachhang.testkh();
     // testSanPham.testsp();
     // testNhanvien.testnv();
     // testGiaodich.testgd();
     //Leaf.test();
+}
 }
