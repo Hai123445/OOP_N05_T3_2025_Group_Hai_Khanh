9c2cfd2b0b86b5cb7dee4bb44120fd75156e9f40
Hai123445 <24100185@st.phenikaa-uni.edu.vn>
2025-07-02 07:59:31 +0000

    1

--- quan ca phe/src/Main.java
+++ quan ca phe/src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    TestUser.Test();
-    TestRecursion.Test();
+    //TestUser.Test();
+    //TestRecursion.Test();
+    TestTime.test();
     // testKhachhang.testkh();
     // testSanPham.testsp();
     // testNhanvien.testnv();
     // testGiaodich.testgd();
     //Leaf.test();
+}
 }
