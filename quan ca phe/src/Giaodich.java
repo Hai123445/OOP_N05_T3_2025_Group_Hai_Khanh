6f5e7ab894bb422d104ca59ebffcfbec9e82355c
Manx-bit <manx7082@gmail.com>
2025-07-12 10:51:51 +0000

    gfhfgh

--- quan ca phe/src/Giaodich.java
+++ quan ca phe/src/Giaodich.java
@@ -3,21 +3,40 @@ public class Giaodich {
     //bien mieu ta cho giao dich
     String MaGiaoDich;
     String NgayThangNamGiaoDich;
-    String GioGiaoDich;
+    String NhanVienGiaoDich;
     double TongTien;
-
+    int TongSoSp;
 
     //phuong thuc hoat dong
-public Giaodich(String mgd,String ngd,String ggd,double tt){
+public Giaodich(String mgd,String ngd,String nvgd,double tt,int tsp){
     this.MaGiaoDich = mgd;
     this.NgayThangNamGiaoDich = ngd;
-    this.GioGiaoDich = ggd;
-    this.TongTien = tt; 
+    this.NhanVienGiaoDich = nvgd;
+    this.TongTien = tt;
+    this.TongSoSp = tsp; 
 }
+//Read
 public void hienthi(){
     System.out.println("Ma giao dich :"  + MaGiaoDich);
     System.out.println("Ngay thang nam giao dich:" + NgayThangNamGiaoDich);
-    System.out.println("Gio giao dich:" + GioGiaoDich);
+    System.out.println("Gio giao dich:" + NhanVienGiaoDich);
     System.out.println("Tong tien: " + TongTien);
+    System.out.println("Tong so san pham : " + TongSoSp);
 }
+}
+//Update 
+public void suaGd(String mgdMoi,String ngdMoi,String nvgdMoi,double ttMoi,int tspMoi){
+    this.MaGiaoDich = mgdMoi;
+    this.NgayThangNamGiaoDich = ngdMoi;
+    this.NhanVienGiaoDich = nvgdMoi;
+    this.TongTien = ttMoi;
+    this.TongSoSp = tspMoi; 
+}
+//Delete
+public void xoaGd(){
+    this.MaGiaoDich = "";
+    this.NgayThangNamGiaoDich = "";
+    this.NhanVienGiaoDich = "";
+    this.TongTien = 0;
+    this.TongSoSp = 0;
 }
--- quan ca phe/src/Main.java
+++ quan ca phe/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
   public static void main(String[] args) {
+<<<<<<< HEAD
     testNhanvien.testnv();
+=======
+    //testUser.test();
+    //TestRecursion.Test();
+    //TestTime.test();
+    // testKhachhang.testkh();
+     testSanPham.testsp();
+    // testNhanvien.testnv();
+    // testGiaodich.testgd();
+    //Leaf.test();
+>>>>>>> 9225c7c5416f96653a7d4d35feafe43ceaf0ac0d
 }
 }
