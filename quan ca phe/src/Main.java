c79ad8c586d98c84a7963fb3fadab3babd6f6713
duckhanh1301 <nguyenduckhanhhhh@gmail.com>
2025-07-12 09:52:37 +0000

    dfg

--- quan ca phe/src/Main.java
+++ quan ca phe/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
   public static void main(String[] args) {
-    testUser.test();
-    TestRecursion.Test();
-    TestTime.test();
+    //testUser.test();
+    //TestRecursion.Test();
+    //TestTime.test();
     // testKhachhang.testkh();
-    // testSanPham.testsp();
+     testSanPham.testsp();
     // testNhanvien.testnv();
     // testGiaodich.testgd();
     //Leaf.test();
--- quan ca phe/src/SanPham.java
+++ quan ca phe/src/SanPham.java
@@ -1,22 +1,28 @@
 public class SanPham {
-    int MaSp;
-    String TenSp;
-    int GiaSp;
-    // phuong thuc
-
-    // phuong thuc dac biet/constructor co ten trung ten class/ khoi tao Class
-    // SanPham
-    public SanPham(int maSp, String tenSp, int giaSp) {
-        this.MaSp = maSp;
-        this.TenSp = tenSp;
-        this.GiaSp = giaSp;
+    String maSp;
+    String tenSp;
+    int giaSp;
+    
+    public SanPham(String maSp, String tenSp, int giaSp) {
+        this.maSp = maSp;
+        this.tenSp = tenSp;
+        this.giaSp = giaSp;
     }
 
-    public void In() {
-        System.out.println("Ten san pham:"+ TenSp);
+    public void printInfo() {
+        System.out.println("Ten san pham:"+ tenSp);
+        System.out.println("Ma san pham: " + maSp);
+        System.out.println("Gia san pham: " + giaSp);
 
-        System.out.println("Ma san pham: " + MaSp);
-        System.out.println("Gia san pham: " + GiaSp);
+    }
 
+    public void suaSp(String tenMoi, int giaMoi) {
+        this.tenSp = tenMoi;
+        this.giaSp = giaMoi;
+    }
+    public void xoaSp() {
+        this.maSp= "";
+        this.tenSp = "";
+        this.giaSp = 0;
     }
 }
