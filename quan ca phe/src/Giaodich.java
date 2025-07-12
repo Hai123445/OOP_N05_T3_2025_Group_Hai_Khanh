03516fea56ec0f22599db57c63ce194289c04a71
Hai123445 <24100185@st.phenikaa-uni.edu.vn>
2025-07-12 10:07:47 +0000

    1

--- quan ca phe/src/Giaodich.java
+++ quan ca phe/src/Giaodich.java
@@ -15,11 +15,28 @@ public Giaodich(String mgd,String ngd,String nvgd,double tt,int tsp){
     this.TongTien = tt;
     this.TongSoSp = tsp; 
 }
+//Read
 public void hienthi(){
     System.out.println("Ma giao dich :"  + MaGiaoDich);
     System.out.println("Ngay thang nam giao dich:" + NgayThangNamGiaoDich);
     System.out.println("Gio giao dich:" + NhanVienGiaoDich);
     System.out.println("Tong tien: " + TongTien);
-     System.out.println("Tong so san pham : " + TongSoSp);
+    System.out.println("Tong so san pham : " + TongSoSp);
 }
+}
+//Update 
+public void suaGd(String mgdMoi,String ngdMoi,String nvgdMoi,double ttMoi,int tspMoi){
+    this.MaGiaoDich = mgdMoi;
+    this.NgayThangNamGiaoDich = ngdMoi;
+    this.NhanVienGiaoDich = nvgdMoi;
+    this.TongTien = ttMoi;
+    this.TongSoSp = tspMoi; 
+}
+//Delete
+public void xoaGd(){
+    this.MaGiaoDich = "";
+    this.NgayThangNamGiaoDich = "";
+    this.NhanVienGiaoDich = "";
+    this.TongTien = 0;
+    this.TongSoSp = 0;
 }
