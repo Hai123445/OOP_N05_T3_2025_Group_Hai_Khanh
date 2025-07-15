bed3c00a2e2aeb1c6d7fc7eba710af51fc07e1fc
Hai123445 <24100185@st.phenikaa-uni.edu.vn>
2025-07-15 04:57:05 +0000

    testthemgd

--- quan ca phe/test/testGiaodich.java
+++ quan ca phe/test/testGiaodich.java
@@ -1,14 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 public class testGiaodich {
-
-    public static void testgd(){ 
-
-
-
-        Giaodich gd = new Giaodich("GD001", "01/07/2025", "10:00", 5000);
-
-        gd.hienthi();
-
+ static Scanner scanner = new Scanner(System.in);
+ static ArrayList<Giaodich> dsGiaodich = new ArrayList<>();
+    public static void menu(){
+        int n;
+        do{
+        System.out.println("1.Create");
+        System.out.println("2.Read");
+        System.out.println("3.Update");
+        System.out.println("4.Delete");
+        n = scanner.nextInt();
+        Scanner.nextLine();  
+        switch(n){
+            case 1 -> themGiaodich();
+            case 2 -> docGiaodich();
+            case 3 -> suaGiaodich();
+            case 4 -> xoaGiaodich();
+            default -> System.out.println("Lua chon khong hop le");
+        }
+     }
+       while(n != 0);
     }
-
-    
+    public static void themGiaodich(){
+        System.out.print("Nhap ma giao dich");
+        String mgd = scanner.nextLine();
+        System.out.print("Nhap ngay giao dich");
+        String ngd = scanner.nextLine();
+        System.out.print("Nhap nhan vien giao dich");
+        String nvgd = scanner.nextLine();
+        System.out.print("Nhap tong tien");
+        Double tt = scanner.nextDouble();
+        System.out.print("Nhap so luong sp")
+        Int tsp = scanner.nextInt();
+        scanner.nextLine();
+        Giaodich gd = new Giaodich(mgd,ngd,nvgd,tt,tsp);
+        dsGiaodich.add(gd);
+        System.out.println("Them giao dich thanh cong");
+   }
 }
+
