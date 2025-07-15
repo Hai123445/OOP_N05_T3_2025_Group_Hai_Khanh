9a379f6897cf0d61e9e4722aa1d2f4c7541a4780
Hai123445 <24100185@st.phenikaa-uni.edu.vn>
2025-07-15 10:00:30 +0000

    fix confict

--- review/Book.java
+++ review/Book.java
@@ -1,4 +1,5 @@
 public class Book {
+<<<<<<< HEAD
     public int ma;
     public String ten;
 
@@ -12,5 +13,12 @@ public int getId() {
 
     public String getTen() {
         return ten;
+=======
+    public String BookID;
+    public String BookName;
+    public Book (String ID, String Name) {
+        this.BookID = ID;
+        this.BookName = Name;
+>>>>>>> f32b3e812fd7d723d181d01be0b8a20dcf35791c
     }
 }
--- review/Library.java
+++ review/Library.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.List;
 public class Library {
     private final List<Book> books;
@@ -9,3 +10,16 @@ public class Library {
         return books;}
     }
 
+=======
+import Book;
+public class Library {
+    private final List<Book> books;
+    public Library (){}
+        Library(List<Book> b) {
+            this.books = b;
+        }
+        public List<Book> getList() {
+            return books; 
+        }
+    }
+>>>>>>> f32b3e812fd7d723d181d01be0b8a20dcf35791c
