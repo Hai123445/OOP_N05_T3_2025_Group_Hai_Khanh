36d9fd195dc7b15ae50704a0efc1ea59fc3d5b34
duckhanh1301 <nguyenduckhanhhhh@gmail.com>
2025-07-15 09:20:32 +0000

    sa

---
+++ review/Book.java
@@ -0,0 +1,8 @@
+public class Book {
+    public String BookID;
+    public String BookName;
+    public Book (String ID, String Name) {
+        this.BookID = ID;
+        this.BookName = Name;
+    }
+}
