7a785c2a5bc7578bb0df420cbc3abb6d5b478243
duckhanh1301 <nguyenduckhanhhhh@gmail.com>
2025-07-15 09:59:45 +0000

    asd

--- review/Book.java
+++
@@ -1,8 +0,0 @@
-public class Book {
-    public String BookID;
-    public String BookName;
-    public Book (String ID, String Name) {
-        this.BookID = ID;
-        this.BookName = Name;
-    }
-}
