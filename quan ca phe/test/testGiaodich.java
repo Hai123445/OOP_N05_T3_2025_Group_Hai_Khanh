2a2736076dfec6107ee5e0153cc4a92b7bf6b2b3
Hai123445 <24100185@st.phenikaa-uni.edu.vn>
2025-07-16 07:09:20 +0000

    sua

--- quan ca phe/test/testGiaodich.java
+++ quan ca phe/test/testGiaodich.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class testGiaodich {
  static Scanner scanner = new Scanner(System.in);
  static ArrayList<Giaodich> dsGiaodich = new ArrayList<>();
-    public static void menu(){
+    public static void testgd(){
         int n;
         do{
         System.out.println("1.Create");
@@ -14,71 +14,14 @@ public class testGiaodich {
         n = scanner.nextInt();
         scanner.nextLine();  
         switch(n){
-            case 1 : 
-            themGiaodich();
-            break;
-            case 2 : docGiaodich(); break;
-            case 3 : suaGiaodich(); break;
-            case 4 : xoaGiaodich(); break;
+            case 1 : Giaodich.themGiaodich();break;
+            case 2 : Giaodich.docGiaodich(); break;
+            case 3 : Giaodich.suaGiaodich(); break;
+            case 4 : Giaodich.xoaGiaodich(); break;
             case 0 : System.out.println("Thoat menu"); break;
             default : System.out.println("Lua chon khong hop le"); break;
         }
      }
        while(n != 0);
     }
-    //create
-    public static void themGiaodich(){
-        System.out.print("Nhap ma giao dich: ");
-        String mgd = scanner.nextLine();
-        System.out.print("Nhap ngay giao dich: ");
-        String ngd = scanner.nextLine();
-        System.out.print("Nhap nhan vien giao dich: ");
-        String nvgd = scanner.nextLine();
-        System.out.print("Nhap tong tien: ");
-        Double tt = scanner.nextDouble();
-        System.out.print("Nhap so luong sp: ");
-        int tsp = scanner.nextInt();
-        scanner.nextLine();
-        Giaodich gd = new Giaodich(mgd,ngd,nvgd,tt,tsp);
-        dsGiaodich.add(gd);
-        System.out.println("Them giao dich thanh cong");
-   }
-   //read
-   public static void docGiaodich(){
-    for (Giaodich gd : dsGiaodich){
-     gd.hienthi();
-   }
-}
-//update
-   public static void suaGiaodich(){
-    String macansua = scanner.nextLine();
-    for(Giaodich gd : dsGiaodich){
-        if(gd.getMgd().equalsIgnoreCase(macansua)){
-            System.out.print("Nhap ma: ");
-            gd.setMgd(scanner.nextLine());
-            System.out.print("Nhap ngay: ");
-            gd.setNgd(scanner.nextLine());
-            System.out.print("Nhap nhan vien: ");
-            gd.setNvgd(scanner.nextLine());
-            System.out.print("nhap tong tien: ");
-            gd.setTt(scanner.nextDouble());
-            System.out.print("Nhap tong sp: ");
-            gd.setTsp(scanner.nextInt());
-            scanner.nextLine();
-            return; 
-        }
-        System.out.println("khong tim thay ma GD");
-    }
-   }
-   //delete
-   public static void xoaGiaodich(){
-    String macanxoa = scanner.nextLine();
-    for(Giaodich gd : dsGiaodich){
-        if(gd.getMgd().equals(macanxoa)){
-                dsGiaodich.remove(gd);
-              return;
-    }
-    System.err.println("khong tim thay ma GD");
-}
-}  
-}
+   
--- quan ca phe/test/testKhachhang.java
+++ quan ca phe/test/testKhachhang.java
@@ -14,53 +14,14 @@ static Scanner scanner = new Scanner(System.in);
         n = scanner.nextInt();
         scanner.nextLine();  
         switch(n){
-            case 1 : themKhachhang();break;
-            case 2 : docKhachhang(); break;
-            case 3 : suaKhachhang(); break;
-            case 4 : xoaKhachhang(); break;
+            case 1 : KhachHang.themKhachhang();break;
+            case 2 : KhachHang.docKhachhang(); break;
+            case 3 : KhachHang.suaKhachhang(); break;
+            case 4 : KhachHang.xoaKhachhang(); break;
             case 0 : System.out.println("Thoat menu"); break;
             default : System.out.println("Lua chon khong hop le"); break;
         }
      }
        while(n != 0);
     }
-  public static void themKhachhang(){
-     System.out.print("Nhap ma KH");
-     String makh = scanner.nextLine(); 
-     System.out.print("Nhap ten KH");
-     String tenKh = scanner.nextLine();
-     System.out.print("Nhap Sdt");
-     String sdt = scanner.nextLine();
-     KhachHang kh = new KhachHang(makh,tenKh,sdt);
-     dsKhachhang.add(kh);
-  }
-  public static void docKhachhang(){
-   for(KhachHang kh : dsKhachhang ){
-      kh.hienthi();
-   };
-  }
-  public static void suaKhachhang(){
-   String macansua = scanner.nextLine();
-   for(KhachHang kh : dsKhachhang){
-      if(kh.getMakh().equals(macansua)){
-         System.out.print("Nhap ma moi");
-         kh.setMakh(scanner.nextLine());
-         System.out.print("Nhap ten moi");
-         kh.setTenkh(scanner.nextLine());
-         System.out.print("Nhap sdt moi");
-         kh.setSdt(scanner.nextLine());
-      }
-      System.out.println("khong tim thay ma");
-   }
-  }
-  public static void xoaKhachhang(){
-      String macanxoa = scanner.nextLine();
-      for(KhachHang kh : dsKhachhang){
-         if(kh.getMakh().equals(macanxoa)){
-            dsKhachhang.remove(kh);
-            break;
-         }
-         System.out.println("Khong tim thay ma");
-      }
-  }  
-}
+   
