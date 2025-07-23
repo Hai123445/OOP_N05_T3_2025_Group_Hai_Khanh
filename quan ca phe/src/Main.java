819d054dcdee3f425b2ea525d12fc92004435134
Nguyễn Đức Khanh <nguyenduckhanhhhh@gmail.com>
2025-07-23 08:31:26 +0000

    r

--- quan ca phe/src/Main.java
+++ quan ca phe/src/Main.java
@@ -1,16 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    //testUser.test();
-   // TestRecursion.Test();
-    //TestTime.test();
-    // testKhachhang.testkh();
-     //testSanPham.testsp();
-     //testNhanvien.menu();
-    //testGiaodich.testgd();
-    //Leaf.test();
-    //testCollection.test(); 
-    //testBook.test();
-    //testSanPham.test();
-  TestStudent.teststudent();
+
 }  
 }
