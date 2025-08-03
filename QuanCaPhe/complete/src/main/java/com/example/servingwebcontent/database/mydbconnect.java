6f7e0d7a0f79bda2e004b7672a35f6e4c1ce421e
Hai123445 <24100185@st.phenikaa-uni.edu.vn>
2025-08-03 13:10:03 +0000

    1

---
+++ QuanCaPhe/complete/src/main/java/com/example/servingwebcontent/database/mydbconnect.java
@@ -0,0 +1,62 @@
+package com.example.servingwebcontent.database;
+
+import org.springframework.stereotype.Controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.lang.Class;
+import java.sql.Statement;
+import org.springframework.beans.factory.annotation.Value;
+
+@Controller
+public class myDBConnection {
+
+    public myDBConnection() {
+    };
+
+    // @Value("${my.database.url}")
+    // protected String myDatabaseURL;
+
+    String myDatabaseURL = "jdbc:mysql://mysql-2954f5bb-opp-data.j.aivencloud.com:14833/defaultdb?sslMode=REQUIRED";
+
+    // @Value("${my.database.driver}")
+    // protected String myDatabaseDriver;
+
+    String myDatabaseDriver = "com.mysql.cj.jdbc.Driver";
+    String username = "avnadmin";
+    private String password = "AVNS_fIeg8rQ_jgkVDcDFWyn";
+
+    public Connection conn = null;
+
+    public Statement getMyConn() {
+
+        try {
+
+            Class.forName(myDatabaseDriver);
+            conn = DriverManager.getConnection(myDatabaseURL);
+            Statement sta = conn.createStatement();
+            return sta;
+
+        } catch (Exception e) {
+            System.out.println("myDBConnection at 34" + e);
+        }
+
+        return null;
+
+    }
+
+    public Connection getOnlyConn() {
+        try {
+            Class.forName(myDatabaseDriver);
+
+            conn = DriverManager.getConnection(myDatabaseURL);
+            return conn;
+
+        } catch (Exception e) {
+            System.out.println("Database connection error: " + e);
+        }
+
+        return conn;
+
+    }
+
+}
