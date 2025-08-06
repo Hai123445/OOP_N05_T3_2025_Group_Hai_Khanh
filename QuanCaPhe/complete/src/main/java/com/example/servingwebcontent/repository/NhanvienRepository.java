c6fa6bfb239a6b0ab6b1afd5fd4f550150c9c886
Nguyễn Đức Khanh <nguyenduckhanhhhh@gmail.com>
2025-08-06 13:15:28 +0000

    ádss

--- QuanCaPhe/complete/src/main/java/com/example/servingwebcontent/repository/NhanvienRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.servingwebcontent.repository;
-
-import com.example.servingwebcontent.model.Nhanvien;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NhanvienRepository extends JpaRepository<Nhanvien, String> {
-}
-
