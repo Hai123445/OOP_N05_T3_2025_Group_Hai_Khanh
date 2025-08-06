75760d05fc506d83b72157a2af2736a279720f2f
Hai123445 <24100185@st.phenikaa-uni.edu.vn>
2025-08-06 13:50:32 +0000

    1

--- QuanCaPhe/complete/src/main/java/com/example/servingwebcontent/database/myDBConnection.java
+++ QuanCaPhe/complete/src/main/java/com/example/servingwebcontent/database/myDBConnection.java
@@ -1,33 +1,43 @@
 package com.example.servingwebcontent.database;
-
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.lang.Class;
 import java.sql.Statement;
 import org.springframework.beans.factory.annotation.Value;
+@Component
+    public class myDBConnection {
 
-@Controller
-public class myDBConnection {
+    @Value("${my.database.url}")
+    private String myDatabaseURL;
+
+    @Value("${my.database.driver}")
+    private String myDatabaseDriver;
+
+    @Value("${my.database.username}")
+    private String myDatabaseUsername;
+
+    @Value("${my.database.password}")
+    private String myDatabasePassword;
 
     public myDBConnection() {
-    };
-
-    // @Value("${my.database.url}")
-    // protected String myDatabaseURL;
-
-    String myDatabaseURL = "jdbc:mysql://mysql-2954f5bb-opp-data.j.aivencloud.com:14833/defaultdb?user=avnadmin&password=AVNS_fIeg8rQ_jgkVDcDFWyn&ssl-mode=REQUIRED";
-;
-
-    // @Value("${my.database.driver}")
-    // protected String myDatabaseDriver;
-
-    String myDatabaseDriver = "com.mysql.cj.jdbc.Driver";
-    
-
+        // Fallback values if properties are not loaded
+        if (myDatabaseURL == null) {
+            myDatabaseURL = "mysql://avnadmin:AVNS_fIeg8rQ_jgkVDcDFWyn@mysql-2954f5bb-opp-data.j.aivencloud.com:14833/defaultdb?ssl-mode=REQUIRED";
+        }
+        if (myDatabaseDriver == null) {
+            myDatabaseDriver = "com.mysql.cj.jdbc.Driver";
+        }
+        if (myDatabaseUsername == null) {
+            myDatabaseUsername = "avnadmin";
+        }
+        if (myDatabasePassword == null) {
+            myDatabasePassword = "AVNS_fIeg8rQ_jgkVDcDFWyn";
+        }
+    }
     public Connection conn = null;
-
-    public Statement getMyConn() {
+    public Statement getMyConn(){
 
         try {
 
