216346513ea6623caec3c895e6cde7f835caca62
Nguyễn Đức Khanh <nguyenduckhanhhhh@gmail.com>
2025-08-10 10:20:36 +0000

    aaaa

--- QuAnlyCoffe/complete/src/main/java/com/example/servingwebcontent/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.servingwebcontent.repository;
-
-import com.example.servingwebcontent.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {}
--- QuAnlyCoffe/complete/src/main/java/com/example/servingwebcontent/repository/PromoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.servingwebcontent.repository;
-
-import com.example.servingwebcontent.model.Promo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromoRepository extends JpaRepository<Promo, Long> {}
